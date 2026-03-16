16278e5b7de20c877916e65c9127e45889a8c374
BelmontRafael <rafaelmagalhaes12323@gmail.com>
2026-03-15 23:25:34 -0300

    feat: wire prisma repository providers module and rest controller

---
+++ src/modules/user/adapters/in/user.controller.ts
@@ -0,0 +1,67 @@
+import { CreateUserDto } from '@/modules/user/dto/create-user.dto'
+import { ListUsersDto } from '@/modules/user/dto/list-users.dto'
+import { UpdateUserDto } from '@/modules/user/dto/update-user.dto'
+import {
+  CREATE_USER_USE_CASE,
+  ICreateUserUseCase,
+} from '@/modules/user/ports/in/ICreateUserUseCase'
+import {
+  DELETE_USER_USE_CASE,
+  IDeleteUserUseCase,
+} from '@/modules/user/ports/in/IDeleteUserUseCase'
+import {
+  GET_USER_USE_CASE,
+  IGetUserUseCase,
+} from '@/modules/user/ports/in/IGetUserUseCase'
+import {
+  IListUsersUseCase,
+  LIST_USERS_USE_CASE,
+} from '@/modules/user/ports/in/IListUsersUseCase'
+import {
+  IUpdateUserUseCase,
+  UPDATE_USER_USE_CASE,
+} from '@/modules/user/ports/in/IUpdateUserUseCase'
+import { Body, Controller, Delete, Get, Inject, Param, Patch, Post, Query } from '@nestjs/common'
+import { ApiTags } from '@nestjs/swagger'
+
+@ApiTags('Users')
+@Controller('users')
+export class UserController {
+  constructor(
+    @Inject(CREATE_USER_USE_CASE)
+    private readonly createUserUseCase: ICreateUserUseCase,
+    @Inject(GET_USER_USE_CASE)
+    private readonly getUserUseCase: IGetUserUseCase,
+    @Inject(LIST_USERS_USE_CASE)
+    private readonly listUsersUseCase: IListUsersUseCase,
+    @Inject(UPDATE_USER_USE_CASE)
+    private readonly updateUserUseCase: IUpdateUserUseCase,
+    @Inject(DELETE_USER_USE_CASE)
+    private readonly deleteUserUseCase: IDeleteUserUseCase,
+  ) {}
+
+  @Post()
+  create(@Body() input: CreateUserDto) {
+    return this.createUserUseCase.execute(input)
+  }
+
+  @Get(':id')
+  getById(@Param('id') id: string) {
+    return this.getUserUseCase.execute(id)
+  }
+
+  @Get()
+  list(@Query() query: ListUsersDto) {
+    return this.listUsersUseCase.execute(query)
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() input: UpdateUserDto) {
+    return this.updateUserUseCase.execute(id, input)
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.deleteUserUseCase.execute(id)
+  }
+}
---
+++ src/modules/user/providers/use-case.providers.ts
@@ -0,0 +1,41 @@
+import { CreateUserUseCase } from '@/modules/user/adapters/in/create-user.usecase'
+import { DeleteUserUseCase } from '@/modules/user/adapters/in/delete-user.usecase'
+import { GetUserUseCase } from '@/modules/user/adapters/in/get-user.usecase'
+import { ListUsersUseCase } from '@/modules/user/adapters/in/list-users.usecase'
+import { UpdateUserUseCase } from '@/modules/user/adapters/in/update-user.usecase'
+import { UserRepository } from '@/modules/user/adapters/out/user.repository'
+import { PasswordService } from '@/modules/user/domain/services/password.service'
+import { CREATE_USER_USE_CASE } from '@/modules/user/ports/in/ICreateUserUseCase'
+import { DELETE_USER_USE_CASE } from '@/modules/user/ports/in/IDeleteUserUseCase'
+import { GET_USER_USE_CASE } from '@/modules/user/ports/in/IGetUserUseCase'
+import { LIST_USERS_USE_CASE } from '@/modules/user/ports/in/IListUsersUseCase'
+import { UPDATE_USER_USE_CASE } from '@/modules/user/ports/in/IUpdateUserUseCase'
+import { USER_REPOSITORY } from '@/modules/user/ports/out/IUserRepository'
+
+export const userProviders = [
+  PasswordService,
+  {
+    provide: CREATE_USER_USE_CASE,
+    useClass: CreateUserUseCase,
+  },
+  {
+    provide: GET_USER_USE_CASE,
+    useClass: GetUserUseCase,
+  },
+  {
+    provide: LIST_USERS_USE_CASE,
+    useClass: ListUsersUseCase,
+  },
+  {
+    provide: UPDATE_USER_USE_CASE,
+    useClass: UpdateUserUseCase,
+  },
+  {
+    provide: DELETE_USER_USE_CASE,
+    useClass: DeleteUserUseCase,
+  },
+  {
+    provide: USER_REPOSITORY,
+    useClass: UserRepository,
+  },
+]
