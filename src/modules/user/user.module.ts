ced7714795b5a5883f18576b896397b7e41095ef
Gabriel <gsmainiere1901@gmail.com>
2026-03-16 23:22:00 -0300

    feat(auth): add jwt dependencies and environment config

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,10 +1,20 @@
+import { environment } from '@/common/config/environment'
 import { Module } from '@nestjs/common'
+import { JwtModule } from '@nestjs/jwt'
 import { PrismaModule } from '../database/prisma.module'
 import { UserController } from './adapters/in/user.controller'
 import { userProviders } from './providers/use-case.providers'
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    JwtModule.register({
+      secret: environment.JWT_SECRET,
+      signOptions: {
+        expiresIn: environment.JWT_EXPIRES_IN_SECONDS,
+      },
+    }),
+  ],
   controllers: [UserController],
   providers: userProviders,
 })
